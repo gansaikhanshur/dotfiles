c0aa54cf8f75321f60c1af1a65edc75fc7c1e34f
gansaikhanshur <gansaikhanshur@gmail.com>
2024-05-26 21:49:08 -0400

    initial commit to add nvim configuration

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,42 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  }
+  use {
+    "nvim-tree/nvim-tree.lua",
+    requires = {
+      'nvim-tree/nvim-web-devicons',
+    }
+  }
+  use 'sainnhe/everforest'
+  use 'rmagatti/alternate-toggler'
+  use 'windwp/nvim-autopairs'
+  use 'mg979/vim-visual-multi'
+  use 'gcmt/wildfire.vim'
+  use 'tpope/vim-surround'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'petertriho/cmp-git'
+  use 'onsails/lspkind-nvim'
+end)
