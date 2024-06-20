16e195eef8c345fd8d3a83b430b66d6199533fed
gansaikhanshur <gansaikhanshur@gmail.com>
2024-06-19 22:14:17 -0400

    enforce ctrl hjkl in insert mode

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -25,3 +25,19 @@ keymap.set("n", "<DOWN>", "<NOP>", opts)
 keymap.set("n", "<LEFT>", "<NOP>", opts)
 keymap.set("n", "<RIGHT>", "<NOP>", opts)
 keymap.set("n", "<RIGHT>", "<NOP>", opts)
+
+keymap.set("v", "<UP>", "<NOP>", opts)
+keymap.set("v", "<DOWN>", "<NOP>", opts)
+keymap.set("v", "<LEFT>", "<NOP>", opts)
+keymap.set("v", "<RIGHT>", "<NOP>", opts)
+
+keymap.set("i", "<UP>", "<NOP>", opts)
+keymap.set("i", "<DOWN>", "<NOP>", opts)
+keymap.set("i", "<LEFT>", "<NOP>", opts)
+keymap.set("i", "<RIGHT>", "<NOP>", opts)
+
+-- use Ctrl + h/j/k/l for movement in insert mode
+keymap.set("i", "<C-h>", "<Left>", opts)
+keymap.set("i", "<C-j>", "<Down>", opts)
+keymap.set("i", "<C-k>", "<Up>", opts)
+keymap.set("i", "<C-l>", "<Right>", opts)
